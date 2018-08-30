4447be58710dd5b31badc89cf8452af7aa7459f0
Sathish Kalaimani (Product Engineering Service) <skalaim@wipro.com>
2018-08-30 19:50:11 +0530

    Multiple Client Code

--- src/main/java/fileUpload/FileUploadWebSocket.java
+++ src/main/java/fileUpload/FileUploadWebSocket.java
@@ -2,6 +2,10 @@ package fileUpload;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
@@ -11,24 +15,29 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/fileupload")
 public class FileUploadWebSocket {
 	
+	static Set<Session> users = Collections.synchronizedSet(new HashSet<Session>());
 	
 	@OnOpen
-	public void handleOpen() {
+	public void handleOpen(Session session) {
 		System.out.println("Client Connected");
+		users.add(session);
 	}
 	
 	@OnMessage
-	public void handleMessage(Session session, ByteBuffer file) throws Exception{
+	public void handleMessage(ByteBuffer file) throws Exception{
 		Charset charset = Charset.forName("ISO-8859-1");
 		String text = charset.decode(file).toString();
 		//System.out.println(text);
+		for(Session s: users) {
+			s.getBasicRemote().sendText(text);	
+		}
 		
-		session.getBasicRemote().sendText(text);
 	}
 	
 	@OnClose
-	public void handleClose() {
+	public void handleClose(Session session) {
 		System.out.println("Client Disconected");
+		users.remove(session);
 	}
 
 }
